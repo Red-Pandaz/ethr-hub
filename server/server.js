3b744cbd431338f50dae739a48ed2841ede9c40a
Red Panda <Deep_Sea_Penguin@protonmail.com>
2024-11-20 14:05:34 -0500

    add .env to .gitignore

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes/routes');
 const cors = require('cors');
-
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express();
 app.use(cors());
 
@@ -13,12 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-// Serve React App
-app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve React App in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+  // For any other route, send back the index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
-const PORT = process.env.PORT || 5000;
+// Use process.env.PORT, if not defined fallback to 5000
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
